6700c39f5bbe4e726d8e461e47d3c8c73b8f9f23
Sithum <sithum.dashantha@gmail.com>
2022-10-16 00:11:04 +0530

    save course endpoint

--- src/api/service/CourseService.js
+++ src/api/service/CourseService.js
@@ -8,6 +8,10 @@ class CourseService {
   async getCourseById(id) {
     return await ApiManager.apiGET(`/api/v1/course/${id}`);
   }
+
+  async saveCourse(courseDetails) {
+    return await ApiManager.apiPOST("/api/v1/course", courseDetails);
+  }
 }
 
 export default new CourseService();
